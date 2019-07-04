2ed8bb83093d7984d9261786a0c468eae4984fa2
C-PN15 <C-PN15@DAL-8CO097-9W7.wks.tsh.thomson.com>
2019-07-04 11:33:29 +0530

    commit

--- src/test/java/Mockito_Project/LibraryTest.java
+++ src/test/java/Mockito_Project/LibraryTest.java
@@ -4,11 +4,28 @@
 package Mockito_Project;
 
 import org.junit.Test;
+
+
+
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+    @Test
+    public void testStaticMethod()
+    {
+    	PowerMockito.mockStatic(PowerMockitoStaticMethod.class);
+    	int num1=10;
+    	int num2=20;
+    	int except=30;
+    	PowerMockito.when(PowerMockitoStaticMethod.add(num1,num2)).thenReturn(except);
+    	int actual =PowerMockitoStaticMethod.add(num1,num2);
+    	Assert.assertEquals(except, actual);
+
     }
 }
